deb80106ca4cf384c4764867d36edd489dc89661
Makan DIANKA <makan.dianka@hotmail.com>
2023-12-24 18:03:08 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,7 @@ RUN apt-get update && apt-get install vim -y
 
 WORKDIR /var/www/
 ADD python3 .
+
+WORKDIR /var/www/web
+
+RUN pip install -r requirements.txt
--- python3/web/web/settings.py
+++ python3/web/web/settings.py
@@ -17,7 +17,7 @@ STRIPE_LIVE_KEY=os.environ.get("STRIPE_LIVE_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.43', '127.0.0.1']
 
 
 # Application definition
